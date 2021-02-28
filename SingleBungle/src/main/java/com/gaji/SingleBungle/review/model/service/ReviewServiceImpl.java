bb0a473f1404b55023567cde6d28b93ddc7502dd
devboryung <kangbr16@naver.com>
2021-02-28 22:03:41 +0900

    [update] mybatis-config(후기게시판) + 후기게시판 조금 작업중,,,

--- SingleBungle/src/main/java/com/gaji/SingleBungle/review/model/service/ReviewServiceImpl.java
+++ SingleBungle/src/main/java/com/gaji/SingleBungle/review/model/service/ReviewServiceImpl.java
@@ -1,5 +1,23 @@
 package com.gaji.SingleBungle.review.model.service;
 
-public class ReviewServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.gaji.SingleBungle.review.model.dao.ReviewDAO;
+import com.gaji.SingleBungle.review.model.vo.ReviewPageInfo;
+
+@Service
+public class ReviewServiceImpl implements ReviewService   {
+
+	
+	private ReviewDAO dao;
+	
+	
+	@Override
+	public ReviewPageInfo getPageInfo(int cp) {
+		
+		int listCount = dao.getListCount();
+		
+		return new ReviewPageInfo(cp,listCount);
+	}
 
 }
