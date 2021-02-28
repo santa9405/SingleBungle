56eca33cf38373841d806a20e50588218d1cd38e
rlagus <rlagus@121.160.168.90>
2021-03-01 02:16:58 +0900

    공지사항 게시글 상세 조회

--- SingleBungle/src/main/java/com/gaji/SingleBungle/admin/controller/adminController.java
+++ SingleBungle/src/main/java/com/gaji/SingleBungle/admin/controller/adminController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gaji.SingleBungle.admin.service.adminService;
 import com.gaji.SingleBungle.admin.vo.AAttachment;
@@ -147,9 +150,41 @@ public class adminController {
 	
 	
 	
-	@RequestMapping("noticeView")
-	public String noticeView() {
-		return "admin/noticeView";
+	@RequestMapping("{boardNo}")
+	public String noticeView(@PathVariable("boardNo") int boardNo,
+			Model model, @RequestHeader(value="referer",required=false) String referer,
+			RedirectAttributes ra) {
+		
+		System.out.println(boardNo);
+		
+		int type=3;
+		ABoard board = service.selectBoard(boardNo,type);
+		
+		String url = null;
+		
+		if(board!=null) {  //  상세 조회 성공 시
+					
+					// 상세 조회 성공한 게시물의 이미지 목록을 조회하는 Service 호출
+					List<AAttachment> attachmentList = service.selectAttachmentList(boardNo);
+					
+					if(attachmentList !=null & !attachmentList.isEmpty()) {
+						model.addAttribute("attachmentList",attachmentList);
+					}
+					
+					model.addAttribute("board",board);
+					url = "admin/noticeView";
+				}else { 
+					
+					if(referer == null) {// 이전 요청 주소가 없는 경우(ex. 북마크나 , 주소창으로 바로 접근을 했을 때)
+						url="redirect:../";
+					}else {// 이전 요청 주소가 있는 경우(ex. 사이트에서 루트를 타고 정상적으로 접근 했을 때)
+						url = "redirect:" + referer;
+					}
+					ra.addFlashAttribute("swalIcon","error");
+					ra.addFlashAttribute("swalTitle","존재하지 않는 게시글입니다.");
+				}
+				return url;
+
 	}
 	
 }
