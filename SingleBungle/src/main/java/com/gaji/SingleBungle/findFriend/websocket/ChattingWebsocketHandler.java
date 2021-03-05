ed566272182f82a2939392e0c3fcb143a8acaa75
user1 <user1@user1-PC>
2021-03-05 10:38:23 +0900

    servlet-context.xml WebSocket 주석 해제, 친구찾기 채팅 진행 중..

--- SingleBungle/src/main/java/com/gaji/SingleBungle/findFriend/websocket/ChattingWebsocketHandler.java
+++ SingleBungle/src/main/java/com/gaji/SingleBungle/findFriend/websocket/ChattingWebsocketHandler.java
@@ -1,14 +1,62 @@
 package com.gaji.SingleBungle.findFriend.websocket;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.gaji.SingleBungle.findFriend.model.service.FindFriendService;
+import com.gaji.SingleBungle.member.model.vo.Member;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class ChattingWebsocketHandler extends TextWebSocketHandler {
 	
 	@Autowired
 	private FindFriendService service;
+	
+	private Logger logger = LoggerFactory.getLogger(ChattingWebsocketHandler.class);
+	
+	private Set<WebSocketSession> sessions
+		= Collections.synchronizedSet(new HashSet<WebSocketSession>());
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		
+		logger.info("{}연결됨", session.getId());
+		
+		sessions.add(session);
+		
+	}
+	
+	// 클라이언트로부터 텍스트 메세지를 받았을 때 수행
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		logger.info("전달받은 내용 : " + message.getPayload());
+		
+		JsonObject obj = new Gson().fromJson(message.getPayload(), JsonObject.class);
+		
+		logger.info("채팅 작성자 : " + obj.get("memberId").toString());
+		logger.info("채팅 내용 : " + obj.get("chat").toString());
+		
+		for(WebSocketSession s : sessions) {
+			
+			// 현재 접속 중인 회원 아이디 얻어오기
+			String loginMemberId = ((Member)s.getAttributes().get("loginMember")).getMemberId();
+			logger.info(loginMemberId + "회원에게 채팅 전달");
+			
+			s.sendMessage(new TextMessage(message.getPayload()));
+			
+		}
+		
+	}
 	
 
 }
