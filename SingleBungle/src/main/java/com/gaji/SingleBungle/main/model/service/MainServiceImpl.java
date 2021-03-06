a9b7201bb412d414a26be738d080c1e685f670ed
sl631 <sl631@LAPTOP-LU3QIRKS>
2021-03-06 21:36:06 +0900

    메인서비스 빈등록 ㅈㅅㅈㅅ;;

--- SingleBungle/src/main/java/com/gaji/SingleBungle/main/model/service/MainServiceImpl.java
+++ SingleBungle/src/main/java/com/gaji/SingleBungle/main/model/service/MainServiceImpl.java
@@ -1,9 +1,11 @@
 package com.gaji.SingleBungle.main.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.gaji.SingleBungle.main.model.dao.MainDAO;
 
+@Service
 public class MainServiceImpl implements MainService {
 	
 
