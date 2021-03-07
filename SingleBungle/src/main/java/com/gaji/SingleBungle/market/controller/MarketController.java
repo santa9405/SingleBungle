0f733f4ac0a84928c11e4b2c9b600f9ebcf3333e
techhan <it_hs@naver.com>
2021-03-08 04:10:38 +0900

    사고팔고 관리자 접근 가능하게 수정

--- SingleBungle/src/main/java/com/gaji/SingleBungle/market/controller/MarketController.java
+++ SingleBungle/src/main/java/com/gaji/SingleBungle/market/controller/MarketController.java
@@ -52,7 +52,7 @@ public class MarketController {
 		String url = null;		
 		
 		if (loginMember != null) {
-			if (loginMember.getMemberGrade().charAt(0) != 'F') {
+			if (loginMember.getMemberGrade().charAt(0) != 'F' || loginMember.getMemberGrade().charAt(0) != 'G') {
 				swalIcon = "error";
 				swalTitle = "사고팔고 게시판은 1등급부터 이용할 수 있습니다.";
 				url = "redirect:/";
