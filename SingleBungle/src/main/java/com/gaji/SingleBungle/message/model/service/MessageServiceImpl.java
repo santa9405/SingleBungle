9c1cebac5996e95cc9398fa191d6fc3f0d44422b
devboryung <kangbr16@naver.com>
2021-03-08 18:03:27 +0900

    [update] 헤더

--- SingleBungle/src/main/java/com/gaji/SingleBungle/message/model/service/MessageServiceImpl.java
+++ SingleBungle/src/main/java/com/gaji/SingleBungle/message/model/service/MessageServiceImpl.java
@@ -1,8 +1,12 @@
 package com.gaji.SingleBungle.message.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gaji.SingleBungle.message.model.vo.Message;
 import com.gaji.SingleBungle.message.model.vo.MessagePageInfo;
 
 @Service
@@ -11,11 +15,17 @@ public class MessageServiceImpl implements MessageService {
 	@Autowired
 	private MessageDAO dao;
 
-	// 전체 쪽지 수 조회
+	// 보낸 쪽지 수 조회 
 	@Override
-	public MessagePageInfo getPageInfo(int cp, int memberNo) {
-		int listCount = dao.getListCount(memberNo);
-		return null;
+	public MessagePageInfo getSendPageInfo(int cp, int memberNo) {
+		int listCount = dao.getSendListCount(memberNo);
+		return new MessagePageInfo(cp, listCount);
+	}
+
+	// 보낸 쪽지 목록 조회
+	@Override
+	public List<Message> selectSendList(Map<String, Object> map) {
+		return dao.selectSendList(map);
 	}
 
 }
